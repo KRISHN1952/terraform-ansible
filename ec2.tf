5d80a4d25102c97c6c32d3f103b1d24b284a362f
KRISHN1952 <57942264+KRISHN1952@users.noreply.github.com>
2020-02-19 17:25:06 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -3,28 +3,20 @@
   secret_key  = "${var.aws_secret_access_key}"
   region      = "us-east-2"
 }
+resource "aws_key_pair" "poc-1" {
 
+  key_name   = "poc-3"
+  public_key = "${var.my_public_key}"
+}
 
-  resource "aws_instance" "ansible" {
+ data "template_file" "init" {
+  template = "${file("${path.module}/userdata.tpl")}"
+}
+
+ resource "aws_instance" "ansible" {
                ami = "ami-0520e698dd500b1d1"
                instance_type = "t2.micro"
                security_groups = ["launch-wizard-1"]
-
-connection {
-               type = "ssh"
-               user = "root"
-               password = ""
-               host = "${aws_instance.ansible.public_ip}"
-               private_key = "${file("./krishna321.pem")}"
-               }
-
-provisioner "local-exec" {
-               command = "echo ${aws_instance.ansible.public_ip} >> public_ip.txt"
-               }
-provisioner "remote-exec" {
-               inline = [ "sudo yum -y update",
-                          "sudo yum -y install epel-repo",
-                          "sudo yum -y install ansible",]
-                          }
-
+               user_data = "${data.template_file.init.rendered}"
+               key_name = "${aws_key_pair.poc-1.id}"
            }
